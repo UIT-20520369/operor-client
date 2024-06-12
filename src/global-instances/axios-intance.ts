e29bf16bca655fac77c644d39229a86ebb5daa86
khanhan2k2eliai <84655671+anbuibz54@users.noreply.github.com>
2024-06-12 21:58:28 +0700

    ready to deploy

--- src/global-instances/axios-intance.ts
+++ src/global-instances/axios-intance.ts
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export const oapi = axios.create({baseURL:'http://localhost:3001/'})
+export const oapi = axios.create({baseURL:'https://operor-take-home-ans-projects-17818e68.vercel.app/'})
